3848ed1c66268fd6a8044c9b4de402dd29a5aec4
Makarova Darya <makarovadarya2704@gmail.com>
2023-03-31 21:51:51 +0300

    Add project files.

---
+++ Database/DataBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Database
+{
+    class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-JN7GOKK\SQLEXPRESS;Initial Catalog=test;Integrated Sequrity=True");
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+
+            }
+        }
+
+        public void closedConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+
+    }
+}
